bc3814c4abcfafaf50acdaf598ae52761519a8ab
krypto5863 <29824718+krypto5863@users.noreply.github.com>
2024-05-04 14:29:01 -0400

    Created the BSONPacker

---
+++ i18nEx.ZIPBsonPacker/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.IO.Compression;
+using System.Text;
+using Newtonsoft.Json.Bson;
+using JsonSerializer = Newtonsoft.Json.JsonSerializer;
+
+namespace i18nEx.ZipBsonPacker
+{
+	
+	internal class Program
+	{
+
+		private static readonly FolderBrowserDialog FolderDialog = new();
+
+		[STAThread]
+		static void Main(string[] args)
+		{
+			var folderDialogResult = FolderDialog.ShowDialog();
+
+			while (folderDialogResult != DialogResult.OK)
+			{
+				folderDialogResult = FolderDialog.ShowDialog();
+			}
+
+			var filesDictionary = new Dictionary<string, byte[]>();
+			var targetDir = FolderDialog.SelectedPath;
+
+			foreach (var file in Directory.EnumerateFiles(targetDir, "*", SearchOption.AllDirectories))
+			{
+				var fileName = Path.GetFileName(file);
+				var textContent = File.ReadAllText(file).Trim();
+
+				Console.WriteLine($"Reading {fileName}...");
+
+				if (filesDictionary.ContainsKey(fileName))
+				{
+					if (fileName.EndsWith(".csv", StringComparison.OrdinalIgnoreCase))
+					{
+						Console.WriteLine($"{fileName} was already declared!! Merging...");
+						filesDictionary[fileName] = Encoding.UTF8.GetBytes(Encoding.UTF8.GetString(filesDictionary[fileName]) + textContent);
+						continue;
+					}
+
+					Console.WriteLine($"{fileName} was already declared!! Skipping...");
+					continue;
+				}
+
+				filesDictionary.Add(fileName, Encoding.UTF8.GetBytes(textContent));
+			}
+
+			var bsonFile = Path.GetFileName(targetDir) + ".bson";
+			var outputPath = Path.Combine(Path.GetDirectoryName(targetDir), bsonFile + ".zip");
+
+			using var fileStream = new FileStream(outputPath, FileMode.Create);
+			using (var archive = new ZipArchive(fileStream, ZipArchiveMode.Create, true))
+			{
+				var bsonFileEntry = archive.CreateEntry(bsonFile);
+
+				using (var entryStream = bsonFileEntry.Open())
+				using (var writer = new BsonDataWriter(entryStream))
+				{
+					var serializer = new JsonSerializer();
+					serializer.Serialize(writer, filesDictionary);
+				}
+			}
+
+			Console.WriteLine($"Done, saved file to {outputPath}");
+		}
+	}
+}
